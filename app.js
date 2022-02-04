8ba1122013765f22e607620e2440f2ac04c8866c
Jhon Ortiz <nickolas.ortiz.r@gmail.com>
2022-02-04 16:31:00 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const boton = document.querySelector('.nav-toggle')
 const openMenu= document.querySelector('.nav-menu')
-// const items= document.querySelector('.nav-menu-link')
+const item1= document.querySelector('.item1')
+const item2= document.querySelector('.item2')
+const item3= document.querySelector('.item3')
 
 
 // Add event listener
@@ -14,6 +16,32 @@ boton.addEventListener('click', ()=>{
     }
 })
 
-// items.addEventListener('click', ()=>{
-//     openMenu.classList.toggle('nav-menu_visible');
-// })
+item1.addEventListener('click', ()=>{
+    openMenu.classList.toggle('nav-menu_visible');
+
+    if(openMenu.classList.contains('nav-menu_visible')){
+        openMenu.setAttribute('aria-label', 'Cerrar menú');
+    } else {
+        openMenu.setAttribute('aria-label', 'Abrir menú')
+    }
+})
+
+item2.addEventListener('click', ()=>{
+    openMenu.classList.toggle('nav-menu_visible');
+
+    if(openMenu.classList.contains('nav-menu_visible')){
+        openMenu.setAttribute('aria-label', 'Cerrar menú');
+    } else {
+        openMenu.setAttribute('aria-label', 'Abrir menú')
+    }
+})
+
+item3.addEventListener('click', ()=>{
+    openMenu.classList.toggle('nav-menu_visible');
+
+    if(openMenu.classList.contains('nav-menu_visible')){
+        openMenu.setAttribute('aria-label', 'Cerrar menú');
+    } else {
+        openMenu.setAttribute('aria-label', 'Abrir menú')
+    }
+})
